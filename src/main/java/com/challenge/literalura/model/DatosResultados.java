a6b5afc965c436d810d43020566e9d6fae3c3080
DiegoMiranda-dev <mirandadiego864@gmail.com>
2024-07-13 15:21:52 -0600

    data mapping from json

---
+++ src/main/java/com/challenge/literalura/model/DatosResultados.java
@@ -0,0 +1,26 @@
+package com.challenge.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosResultados(@JsonAlias("results") List<DatosLibro> resultados) {
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record DatosLibro(
+            @JsonAlias("title") String titulo,
+            @JsonAlias("authors") List<Autor> autores,
+            @JsonAlias("languages") List<String> lenguajes,
+            @JsonAlias("download_count") String descargas
+    ) {
+
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public record Autor(
+                @JsonAlias("name") String nombre
+        ) {
+        }
+
+    }
+}
