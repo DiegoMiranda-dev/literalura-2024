03555bae1f69bce947d9713179498e365dc99190
DiegoMiranda-dev <mirandadiego864@gmail.com>
2024-07-13 15:21:11 -0600

    making api service how public

--- src/main/java/com/challenge/literalura/services/ConsumoApi.java
+++ src/main/java/com/challenge/literalura/services/ConsumoApi.java
@@ -7,7 +7,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsumoApi {
-    private String obtenerDatos(String url) {
+    public String obtenerDatos(String url) {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
         HttpResponse<String> response = null;
